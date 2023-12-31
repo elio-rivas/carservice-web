db886942c156014aee4f1ce68516e49476a15c79
ElioRivas <eliocarmen@hotmail.com>
2023-12-30 18:58:02 -0600

    uploading images

---
+++ src/app/containers/HomePage/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+
+const PageContainer = styled.div``;
+
+export function Homepage(){
+    return <PageContainer>
+        </PageContainer>
+}
